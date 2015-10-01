a22d0668c6a8773f7fd63c064c7e52a87693da21
linbo lai <1234wbzh@sina.com>
2015-10-01 22:38:06 +0800

    Trasfer to SQLite

--- ClientForm/CurrentUser.cs
+++ ClientForm/CurrentUser.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Data.SqlClient;
+using System.Data.SQLite;
 using System.Data;
 using System.Windows.Forms;
 
@@ -9,14 +9,14 @@ namespace ClientForm
 {
      static class CurrentUser
     {
-        static string connectStr = @"Data Source=(LocalDB)\v11.0;AttachDbFilename="+Application.StartupPath+ @"\Database1.mdf;Integrated Security=True";
-        static SqlConnection objConnection = new SqlConnection(connectStr);
+        static SQLiteConnection objConnection = new SQLiteConnection(@"Data Source=UserData.sql;Version=3;");
         static public DataRow currentUser ;
+        
 
         static public DataRow getProtype()
         {
-            string cmdstring = "select * from [dbo].[userData] where Username = 'a'";
-            SqlDataAdapter sa = new SqlDataAdapter(cmdstring, objConnection);
+            string cmdstring = "select * from userdata where Username = 'a'";
+            SQLiteDataAdapter sa = new SQLiteDataAdapter(cmdstring, objConnection);
             DataTable dtOut = new DataTable();
             sa.Fill(dtOut);
             return dtOut.NewRow();
@@ -24,8 +24,8 @@ namespace ClientForm
         static public bool Check(string username)
         {
             objConnection.Open();
-            string cmdstring = "select Username from [dbo].[userData] where Username = '" + username + "'";
-            SqlCommand sqlcmd = new SqlCommand(cmdstring,objConnection);
+            string cmdstring = "select Username from userdata where Username = '" + username + "'";
+            SQLiteCommand sqlcmd = new SQLiteCommand(cmdstring,objConnection);
             try
             {
                  string result = (string)sqlcmd.ExecuteScalar();
@@ -40,7 +40,7 @@ namespace ClientForm
                      return true;
                  }
             }
-            catch (System.Data.SqlClient.SqlException)
+            catch (System.Data.SQLite.SQLiteException)
             {
                 objConnection.Close();
                 return false;
@@ -50,12 +50,12 @@ namespace ClientForm
 
         static public DataTable LogIn(DataTable dtIn)
         {
-            string cmdstring = "select * from [dbo].[userData]";
-            SqlDataAdapter sa = new SqlDataAdapter(cmdstring, objConnection);
+            string cmdstring = "select * from userdata";
+            SQLiteDataAdapter sa = new SQLiteDataAdapter(cmdstring, objConnection);
             DataTable dtOut = new DataTable();
             sa.Fill(dtOut);
             dtOut.Rows.Add(dtIn.Rows[0].ItemArray);
-            SqlCommandBuilder objCommandBuilder = new SqlCommandBuilder(sa);
+            SQLiteCommandBuilder objCommandBuilder = new SQLiteCommandBuilder(sa);
             sa.Update(dtOut);
             currentUser = dtIn.Rows[0];
             objConnection.Close();
@@ -64,12 +64,12 @@ namespace ClientForm
 
         static public void Update(DataRow dr)
         {
-            string cmdstring = "select * from [dbo].[userData] where Username ='" + currentUser["Username"] + "'";
-            SqlDataAdapter sa = new SqlDataAdapter(cmdstring, objConnection);
+            string cmdstring = "select * from userdata where Username ='" + currentUser["Username"] + "'";
+            SQLiteDataAdapter sa = new SQLiteDataAdapter(cmdstring, objConnection);
             DataTable dtOut = new DataTable();
             sa.Fill(dtOut);
             dtOut.Rows[0].ItemArray = dr.ItemArray;
-            SqlCommandBuilder objCommandBuilder = new SqlCommandBuilder(sa);
+            SQLiteCommandBuilder objCommandBuilder = new SQLiteCommandBuilder(sa);
             sa.Update(dtOut);
             currentUser = dr;
             objConnection.Close();
@@ -77,8 +77,8 @@ namespace ClientForm
 
         static public void SignIn(string username, string password)
         {
-            string cmdstring = "select * from [dbo].[userData] where Username = '" + username + "'and Password = '" + password + "'";
-            SqlDataAdapter sa = new SqlDataAdapter(cmdstring, objConnection);
+            string cmdstring = "select * from userdata where Username = '" + username + "'and Password = '" + password + "'";
+            SQLiteDataAdapter sa = new SQLiteDataAdapter(cmdstring, objConnection);
             DataTable dt = new DataTable();
             sa.Fill(dt);
             if (dt.Rows.Count == 0)
