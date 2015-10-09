dac10522edef0fbaf2aea5aa8af8953fc6c46a5d
linbo lai <1234wbzh@sina.com>
2015-10-09 09:14:38 +0800

    split wavedraw & serial data

--- ClientForm/WaveDraw.cs
+++ ClientForm/WaveDraw.cs
@@ -29,68 +29,7 @@ namespace ClientForm
         float[] buffer = new float[5000];//缓冲区
         int num = 0;//缓冲区数据计数
 
-        private void spInit()
-        {
-            sp = new SerialPort();
-            sp.PortName = "COM40";
-            sp.BaudRate = 57600;
-            sp.DataBits = 8;
-            sp.Parity = Parity.Even;
-            try
-            {
-                sp.Open();
-                try
-                {
-                    sp.DataReceived += new SerialDataReceivedEventHandler(sp_DataReceived);//事件
-
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show(ex.Message);
-                }
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("无法连接串口");
-            }
-            
-            
-
-        }
-
-        public void sp_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
-        {
-            this.Invoke(new MethodInvoker(delegate()
-            {
-                if (sp.IsOpen)
-                {
-                    // int a=sp.ReadBufferSize;
-                    byte[] sp_buffer = new byte[sp.ReadBufferSize + 1];
-
-                    int count = sp.Read(sp_buffer, 0, sp.ReadBufferSize);
-
-                    for (int i = 0; i < count; i++)
-                    {
-                        float receivedrawWaveData = parser.parseByte(sp_buffer[i]);
-
-                        if (receivedrawWaveData != 0)
-                        {
-                            receivedrawWaveData = receivedrawWaveData * 0.21973F;//转化为实际电压值，微伏
-                            ///////////////////////////
-                            // lock (buffer)
-                            //{
-                            
-                            buffer[num++] = receivedrawWaveData;
-                            
-                             //}
-                                 //写数据到txt
-
-                            ////////////////                           
-                        }
-                    }
-                }
-            }));
-        }
+       
         public WaveDraw()
         {
             InitializeComponent();
